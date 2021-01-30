c88571b72dcd88a6279333450db546f932fe3c58
rashed01 <rashadulislam9@gmail.com>
2021-01-30 19:14:43 +0600

    mongodb uri updated

--- config/connectDB.js
+++ config/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0-bookfinder.xotfu.mongodb.net/bookfinder`,
+  mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.bg2dp.mongodb.net/bookfinder?retryWrites=true&w=majority`,
     {
       useUnifiedTopology: true,
       useNewUrlParser: true,
