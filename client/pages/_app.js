988e8b546633b39b04c0c462fd5ef5e657328323
rashed01 <rashadulislam9@gmail.com>
2021-02-06 18:57:55 +0600

    react client added

--- client/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "react-datepicker/dist/react-datepicker.css";
-import "../styles/master.scss";
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
